1589fab06b536d4bf37165b1810f61fa36ef8e6b
iam-shalu <163649503+iam-shalu@users.noreply.github.com>
2025-10-27 14:24:55 +0530

    Update CityBlackList.java

--- src/main/java/PG10PageObject/CityBlackList.java
+++ src/main/java/PG10PageObject/CityBlackList.java
@@ -95,7 +95,7 @@ public class CityBlackList {
             WebElement blackListUpload = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='fileInput']")));
             // String filePath = "D:\\Automation\\pg10-automation\\Upload Excel File\\CityBlackList Customer\\CityBlackList.xlsx";
            // String filePath = System.getProperty("user.dir") + "/src/test/resources/excel/CityBlackList Customer/CityBlackList.xlsx";
-            String filePath = System.getProperty("user.dir") + "Upload Excel File/CityBlackList Customer/CityBlackList.xlsx";
+            String filePath = System.getProperty("user.dir") + "/Upload Excel File/CityBlackList Customer/CityBlackList.xlsx";
             blackListUpload.sendKeys(filePath);
             System.out.println("✅ File uploaded successfully.");
 
